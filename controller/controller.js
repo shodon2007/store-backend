8296dcc5c070e925c4979997088a114012c77d8d
shodon <about@shodon.ru>
2023-08-17 08:44:56 +0700

    added brands filter

--- controller/controller.js
+++ controller/controller.js
@@ -14,7 +14,26 @@ class Controller {
     async getType(req, res) {
         const query = 'SELECT * FROM type';
         const data = await queryDatabase(query);
-        console.log(data);
+        res.json(data);
+    }
+    async getProducts(req, res) {
+        const { type } = req.query;
+        const query = `
+        SELECT product.name, product.price, brand.name AS "brand"FROM product 
+        INNER JOIN type ON product.type_id = type.id
+        INNER JOIN brand ON product.brand_id = brand.id
+        WHERE type.name = "${type}"`;
+        const data = await queryDatabase(query);
+        res.json(data);
+    }
+    async getBrand(req, res) {
+        const { type } = req.query;
+        const query = `
+        SELECT DISTINCT brand.id, brand.name from brand 
+        INNER JOIN product ON brand.id = product.brand_id
+        INNER JOIN type ON product.type_id = type.id
+        WHERE type.name = "${type}"`;
+        const data = await queryDatabase(query);
         res.json(data);
     }
 }
--- router/index.js
+++ router/index.js
@@ -5,5 +5,7 @@ const controller = require('../controller/controller.js');
 const router = new Router();
 
 router.get('/type', controller.getType)
+router.get('/product', controller.getProducts)
+router.get('/getBrand', controller.getBrand)
 
 module.exports = router;
